22cab85126d7a0ce080b12f26587fc9253998579
yani27 <yanilacamora@gmail.com>
2021-01-31 21:20:31 +0100

    add timer for pagination

--- scrap.py
+++ scrap.py
@@ -1,15 +1,28 @@
+from IPython.core.display import clear_output
 import bs4
 import json
 import requests
 import re
 from urllib.request import urlopen as ureq
 from bs4 import BeautifulSoup as soup
-
+from time import sleep
+from random import randint
+from time import timestart_time = time()
 
 import os
 BASE_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
 CSV_FILE = os.path.join(BASE_DIRECTORY, 'data_csv.csv')
 JSON_FILE = os.path.join(BASE_DIRECTORY, 'data_json.json')
+
+start_time = time()requests = 0
+for _ in range(5):
+    # A request would go here
+    requests += 1
+    sleep(randint(1, 3))
+    current_time = time()
+    elapsed_time = current_time - start_time
+    print('Request: {}; Frequency: {} requests/s'.format(requests, requests/elapsed_time))
+clear_output(wait=True)
 
 data_json = []
 data_csv = "name_product,discription_product,colors_product,price_product,catégorie\n"
